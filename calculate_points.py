5075233a0ff686d748b92d0905cc2f26e2b83871
Akhil Ravipati <akhil.ravipati.o@gmail.com>
2022-02-09 23:09:53 -0500

    Add courseworks id integration and sorting

--- calculate_points.py
+++ calculate_points.py
@@ -6,7 +6,7 @@ from util.common import fail
 from util.log import get_logger
 from grader import CONTEST_PLATFORMS, PRACTICE_PLATFORMS
 import json
-from csv import DictWriter
+from csv import DictWriter, DictReader
 
 
 LOG = get_logger("CalculatePoints")
@@ -22,15 +22,31 @@ def parse_args():
 def calculate(week_num: int):
     grade_sheet_path = CACHE_PATH.joinpath(f"grades_{week_num}.csv")
     grade_events_path = CACHE_PATH.joinpath(f"grade_{week_num}.log")
+    uni_to_courseworks_path = CACHE_PATH.joinpath("uni_courseworks_id.csv")
     
     if not grade_events_path.exists():
         fail(f"Grading events file: [{grade_events_path}] does not exist", LOG)
+
+    if not uni_to_courseworks_path.exists():
+        fail(f"Uni to courseworks file: [{uni_to_courseworks_path}] does not exist", LOG)
 
     if grade_sheet_path.exists():
         LOG.warning(f"Grade sheet: [{grade_sheet_path}] already exists")
         val = input("Overwrite? (y/n): ")
         if val != "y":
             exit(-1)
+
+    # Create uni to courseworks id map
+    uni_to_courseworks_id = dict()
+    uni_to_name = dict()
+    name_idx = dict()
+    with open(uni_to_courseworks_path, 'r', encoding='utf-8', newline='') as f:
+        reader = DictReader(f)
+        for i, row in enumerate(reader):
+            print(row)
+            uni_to_courseworks_id[row["uni"]] = row["courseworks_id"]
+            uni_to_name[row["uni"]] = row["name"]
+            name_idx[row["name"]] = i
 
     # Gather into map
     usr_points_map = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
@@ -49,44 +65,49 @@ def calculate(week_num: int):
     if num_exceptions:
         LOG.error(f"There are [{num_exceptions}] exceptions in grade log!!")
 
-    # finalize headers
+    # finalize headers.
+    # final_headers should be ordered in the way we wanna see in the csv
     contest_platform_headers = sorted([platform.name() for platform in CONTEST_PLATFORMS])
     practice_platform_headers = sorted([platform.name() for platform in PRACTICE_PLATFORMS])
 
-    final_headers = ["uni"]
+    final_headers = ["uni", "name", "courseworks_id"]
+    final_headers += ["final_points", "comments"]
+    final_headers += ["total_contest_points", "total_practice_points", "total_points"]
     final_headers += [ct + "_contest" for ct in contest_platform_headers]
     final_headers += [pt + "_practice" for pt in practice_platform_headers]
     final_headers += ["Topcoder_contest", "Topcode_practice", "Leetcode_practice", "Kattis_practice"] # manual - auto grader does not cover these yet - dictwriter will fill in 'MANUAL' in each row
-    final_headers += ["contest_points", "practice_points", "total_points"]
 
     # begin creating csv rows as dicts
     rows = []
     for uni, event_type_map in usr_points_map.items():
-        row = {"uni": uni}
+        row = {"uni": uni, "name": uni_to_name[uni], "courseworks_id": uni_to_courseworks_id[uni]}
 
         contest_points = 0
         for platform_name in contest_platform_headers:
             row[platform_name + "_contest"] = event_type_map["contest"][platform_name]
             contest_points += event_type_map["contest"][platform_name]
-        row["contest_points"] = contest_points
+        row["total_contest_points"] = contest_points
 
         practice_points = 0
         for platform_name in practice_platform_headers:
             row[platform_name + "_practice"] = event_type_map["practice"][platform_name]
             practice_points += event_type_map["practice"][platform_name]
-        row["practice_points"] = practice_points
+        row["total_practice_points"] = practice_points
 
         row["total_points"] = contest_points + practice_points
         rows.append(row)
 
 
     # write the rows into csv
+    # NOTE: Courseworks outputs submissions ordered by name as seen in the uni_courseworks_id.csv
+    #       So, we gotta sort our rows similarly.
+    rows.sort(key=lambda x: name_idx[x["name"]])
     with open(grade_sheet_path, "w", newline='', encoding='utf-8') as f:
         writer = DictWriter(f, fieldnames=final_headers, restval='MANUAL')
         writer.writeheader()
         writer.writerows(rows)
 
-    LOG.info("Done.")
+    LOG.info(f"Done. See file: {grade_events_path}")
 
         
 
